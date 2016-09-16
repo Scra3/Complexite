bb9e39c85646fea48d5589a0ccca54f1c7d3a375
scra <b16007026@tempo21-2.luminy.univ-amu.fr>
2016-09-16 16:30:42 +0200

    question 3 division

--- TP1/TP1/src/tp1/TP1.java
+++ TP1/TP1/src/tp1/TP1.java
@@ -18,7 +18,7 @@ public class TP1 {
      * @param args the command line arguments
      */
     public Map algorithmeSimple(int tableau[]) {
-        Map<String, Integer> indices = new HashMap<String, Integer>(); 
+        Map<String, Integer> indices = new HashMap<String, Integer>();
         int sauvegarde = 0; // permet de sauvegarde la valeur max de la séquence
         int resultat = 0; // variable tampon stockant le resultat de la séquence
         int l, i, j;
@@ -40,7 +40,7 @@ public class TP1 {
     }
 
     public Map algorithmeOptimise(int tableau[]) {
-        Map<String, Integer> indices = new HashMap<String, Integer>(); 
+        Map<String, Integer> indices = new HashMap<String, Integer>();
         int sauvegarde = 0; // permet de sauvegarde la valeur max de la séquence
         int resultat = 0; // variable tampon stockant le resultat de la séquence
         int l, i, j;
@@ -55,6 +55,82 @@ public class TP1 {
                 }
             }
             resultat = 0;
+        }
+        return indices;
+    }
+
+    public Map diviserRegnerMax(int tableau[]) {
+        Map<String, Integer> indices = new HashMap<String, Integer>();
+        int[] T1 = null;
+        int[] T2 = null;
+
+        if (tableau.length <= 1) {
+            // calculer Max
+            indices = algorithmeOptimise(tableau);
+        } else {
+            if (tableau.length > 1 && tableau.length % 2 == 0) {
+
+                T1 = new int[(int) Math.floorDiv(tableau.length, 2)];
+                T2 = new int[tableau.length / 2];
+
+                for (int i = 0; i < Math.floorDiv(tableau.length, 2); i++) {
+                    T1[i] = tableau[i];
+                }
+
+                int a = 0;
+                for (int i = tableau.length - 1; i >= tableau.length / 2; i--) {
+                    T2[a] = tableau[i];
+                    a++;
+                }
+                for (int i = 0;
+                        i < tableau.length / 2; i++) {
+                    System.out.print(" " + T1[i] + " ");
+
+                }
+
+                System.out.println("");
+
+                for (int i = 0;
+                        i < tableau.length / 2; i++) {
+                    System.out.print(" " + T2[i] + " ");
+
+                }
+
+                System.out.println("");
+
+            }
+            if (tableau.length > 1 && tableau.length % 2 != 0) {
+
+                T1 = new int[Math.floorDiv(tableau.length, 2)];
+                T2 = new int[Math.floorDiv(tableau.length, 2) + 1];
+
+                for (int i = 0; i < Math.floorDiv(tableau.length, 2); i++) {
+                    T1[i] = tableau[i];
+                }
+
+                int a = 0;
+                for (int i = Math.floorDiv(tableau.length, 2); i < tableau.length; i++) {
+                    T2[a] = tableau[i];
+                    a++;
+                }
+                for (int i = 0;
+                        i < tableau.length / 2; i++) {
+                    System.out.print(" " + T1[i] + " ");
+
+                }
+
+                System.out.println("");
+
+                for (int i = 0; i < Math.floorDiv(tableau.length, 2) + 1; i++) {
+                    System.out.print(" " + T2[i] + " ");
+
+                }
+
+                System.out.println("");
+
+            }
+            diviserRegnerMax(T1);
+            diviserRegnerMax(T2);
         }
         return indices;
     }
@@ -74,7 +150,7 @@ public class TP1 {
         T[8] = 86;
         T[9] = -64;
 
-        System.out.println(algos.algorithmeSimple(T));
+        System.out.println(algos.diviserRegnerMax(T));
     }
 
 }
