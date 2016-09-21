2c66501555c5f581d14b7a4d1b71997f6581bd54
scra <albanbertolini@gmail.com>
2016-09-21 11:26:10 +0200

    tp1 fin

--- TP1/TP1/src/tp1/TP1.java
+++ TP1/TP1/src/tp1/TP1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tp1;
 
 import java.util.HashMap;
@@ -18,7 +13,7 @@ public class TP1 {
      * @param args the command line arguments
      */
     public Map algorithmeSimple(int tableau[]) {
-        Map<String, Integer> indices = new HashMap<String, Integer>();
+        Map<String, Integer> sousSequenceT1 = new HashMap<String, Integer>();
         int sauvegarde = 0; // permet de sauvegarde la valeur max de la séquence
         int resultat = 0; // variable tampon stockant le resultat de la séquence
         int l, i, j;
@@ -30,17 +25,17 @@ public class TP1 {
                 }
                 if (sauvegarde < resultat) {
                     sauvegarde = resultat;
-                    indices.put("MAX", i);
-                    indices.put("MIN", l);
+                    sousSequenceT1.put("MAX", i);
+                    sousSequenceT1.put("MIN", l);
                 }
                 resultat = 0;
             }
         }
-        return indices;
+        return sousSequenceT1;
     }
 
     public Map algorithmeOptimise(int tableau[]) {
-        Map<String, Integer> indices = new HashMap<String, Integer>();
+        Map<String, Integer> sousSequenceT1 = new HashMap<String, Integer>();
         int sauvegarde = -100000; // permet de sauvegarde la valeur max de la séquence
         int resultat = 0; // variable tampon stockant le resultat de la séquence
         int l, i, j;
@@ -50,63 +45,39 @@ public class TP1 {
                 resultat = resultat + tableau[i];
                 if (sauvegarde < resultat) {
                     sauvegarde = resultat;
-                    indices.put("MAX", i);
-                    indices.put("MIN", l);
+                    sousSequenceT1.put("MAX", i);
+                    sousSequenceT1.put("MIN", l);
                 }
             }
             resultat = 0;
         }
-        indices.put("SUM", sauvegarde);
-        return indices;
+        sousSequenceT1.put("SUM", sauvegarde);
+        return sousSequenceT1;
     }
-    static Map<String, Integer> tampon = new HashMap<String, Integer>();
 
-    public Map diviserRegnerMax(int tableau[]) {
+    static Map<String, Integer> sequence = new HashMap<String, Integer>();
+
+    public Map diviserRegnerMax(int tableau[], Map<String, Integer> sequenceMax) {
 
         // DECLARATIONS 
-        Map<String, Integer> indices = new HashMap<String, Integer>();
-        Map<String, Integer> indices2 = new HashMap<String, Integer>();
+        Map<String, Integer> sousSequenceT1 = new HashMap<String, Integer>();
+        Map<String, Integer> sousSequenceT2 = new HashMap<String, Integer>();
+        Map<String, Integer> sousSequence = new HashMap<String, Integer>();
 
         int[] T1 = null;
         int[] T2 = null;
         int sum = 0;
         int min = 0;
         int max = 0;
-        tampon.put("SUM", 0);
+        int mediane = 0;
+
         //TRAITEMENTS
-        if (tableau.length < 2){
-            indices = algorithmeOptimise(tableau);
-            System.out.println(indices);
-            // récupérer valeurs 
-            sum = indices.get("SUM");
-            min = indices.get("MIN");
-            max = indices.get("MAX");
-
-            if (max + tampon.get("SUM") > tampon.get("SUM") && max + tampon.get("SUM") > max) {
-                // save sum
-                tampon.put("SUM", tampon.get("SUM") + max);
-                // save indices
-                if (tampon.get("MIN") < min) {
-                    tampon.put("MIN", tampon.get("MIN"));
-                } else {
-                    tampon.put("MIN", min);
-                }
-
-                if (tampon.get("MAX") > max) {
-                    tampon.put("MIN", tampon.get("MAX"));
-                } else {
-                    tampon.put("MAX", max);
-                }
-
-            } else if (max + tampon.get("SUM") < max) {
-                // save sum
-                tampon.put("SUM", max);
-                // save indices
-                tampon.put("MIN", min);
-                tampon.put("MAX", max);
-            }
+        if (tableau.length < 4) {
+            sousSequenceT1 = algorithmeOptimise(tableau);
         } else {
-            if (tableau.length > 1 && tableau.length % 2 == 0) {
+            if (tableau.length % 2 == 0) {
+
+                mediane = tableau.length / 2 - 1;
 
                 T1 = new int[(int) Math.floorDiv(tableau.length, 2)];
                 T2 = new int[tableau.length / 2];
@@ -114,10 +85,17 @@ public class TP1 {
                 for (int i = 0; i < Math.floorDiv(tableau.length, 2); i++) {
                     T1[i] = tableau[i];
                 }
-
                 int a = 0;
+
+                for (int i = Math.floorDiv(tableau.length, 2); i < tableau.length; i++) {
+                    T2[a] = tableau[i];
+                    a++;
+                }
+
             }
-            if (tableau.length > 1 && tableau.length % 2 != 0) {
+            if (tableau.length % 2 != 0) {
+
+                mediane = Math.floorDiv(tableau.length, 2);
 
                 T1 = new int[Math.floorDiv(tableau.length, 2)];
                 T2 = new int[Math.floorDiv(tableau.length, 2) + 1];
@@ -133,15 +111,81 @@ public class TP1 {
                 }
             }
 
-            diviserRegnerMax(T1);
-            diviserRegnerMax(T2);
+            sousSequenceT1 = diviserRegnerMax(T1, sequenceMax);
+            sousSequenceT1 = caculerMediane(true, sousSequenceT1, mediane, tableau.length);
+            sousSequenceT2 = diviserRegnerMax(T2, sequenceMax);
+            sousSequenceT2 = caculerMediane(false, sousSequenceT2, mediane, tableau.length);
+            sousSequence = calculerChevauchement(tableau, sousSequenceT1, sousSequenceT2);
+            sauvegarderMax(sousSequence);
         }
-        return tampon;
+        return sousSequenceT1;
+    }
+
+    public static Map<String, Integer> caculerMediane(boolean B, Map<String, Integer> sequence_1, int mediane, int tailleTableau) {
+        Map<String, Integer> sequenceMax = new HashMap<String, Integer>();
+        int sum1, min1, max1 = 0;
+
+        max1 = sequence_1.get("MAX");
+        min1 = sequence_1.get("MIN");
+        sum1 = sequence_1.get("SUM");
+
+        //T1
+        if (B == true) {
+            sequenceMax.put("MAX", max1);
+            sequenceMax.put("MIN", min1);
+            sequenceMax.put("SUM", sum1);
+        } else {
+            sequenceMax.put("MAX", mediane + max1);
+            sequenceMax.put("MIN", mediane + min1);
+            sequenceMax.put("SUM", sum1);
+        }
+
+        return sequenceMax;
+    }
+
+    public static void sauvegarderMax(Map<String, Integer> sousSequence) {
+        if (sousSequence.get("SUM") > sequence.get("SUM") || sequence == null) {
+            sequence.put("MAX", sousSequence.get("MAX"));
+            sequence.put("MIN", sousSequence.get("MIN"));
+            sequence.put("SUM", sousSequence.get("SUM"));
+        }
+    }
+
+    public static Map<String, Integer> calculerChevauchement(int tableau[], Map<String, Integer> T1, Map<String, Integer> T2) {
+        Map<String, Integer> sequenceChevauchement = new HashMap<String, Integer>();
+        int sumCh, minCh, maxCh;
+
+        sumCh = 0;
+        minCh = T1.get("MIN");
+        maxCh = T2.get("MAX");
+
+        for (int i = minCh; i <= maxCh; i++) {
+            sumCh = sumCh + tableau[i];
+        }
+
+        sequenceChevauchement.put("SUM", sumCh);
+        sequenceChevauchement.put("MIN", minCh);
+        sequenceChevauchement.put("MAX", maxCh);
+
+        if (sumCh > T1.get("SUM") && sumCh > T2.get("SUM")) {
+            return sequenceChevauchement;
+        } else if (sumCh < T1.get("SUM") && T2.get("SUM") < T1.get("SUM")) {
+            return T1;
+        } else if (sumCh < T2.get("SUM") && T2.get("SUM") > T1.get("SUM")) {
+            return T2;
+        } else {
+            return null;
+        }
     }
 
     public static void main(String[] args) {
         TP1 algos = new TP1();
         // TODO code application logic here
+
+        Map<String, Integer> sequenceMax = new HashMap<>();
+        sequence.put("MAX", 0);
+        sequence.put("MIN", 0);
+        sequence.put("SUM", -100000);
         int T[] = new int[10];
         T[0] = 44;
         T[1] = -54;
@@ -149,12 +193,13 @@ public class TP1 {
         T[3] = 62;
         T[4] = -65;
         T[5] = 82;
-        T[6] = -81;
-        T[7] = -82;
+        T[6] = 81;
+        T[7] = 82;
         T[8] = 86;
-        T[9] = -64;
+        T[9] = 64;
 
-        System.out.println(algos.diviserRegnerMax(T));
+        algos.diviserRegnerMax(T, sequenceMax);
+        System.out.println(sequence);
     }
 
 }
